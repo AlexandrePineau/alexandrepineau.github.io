1305f6f11e117b89bd2cf26db32081c3d9571cb8
Alexandre Pineau <alecpineau@gmail.com>
2020-07-21 19:08:04 -0400

    Cleaned up SErvices

--- module9-lab/my-app/src/Services.js
+++ module9-lab/my-app/src/Services.js
@@ -3,38 +3,56 @@ import React from 'react';
 export const Services = () => (
     <div>
         <div className="container">
-            <h2 id="services">~Our services~</h2>
-            <h3 >Basic website design</h3>
-            <p>Includes:</p>
-            <ul>
-                <li>Choice from 100's of templates</li>
-                <li>1 custom logo</li>
-                <li>Discounted web hosting</li>
-            </ul>
+            <div className="row p-5">
+                <div className="col-lg-6">
+                    <div className="p-5 border border-primary bg-grey">
+                        <h3 >Basic website design</h3>
+                        <p>Includes:</p>
+                        <ul>
+                            <li>Choice from 100's of templates</li>
+                            <li>1 custom logo</li>
+                            <li>Discounted web hosting</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="p-5 border border-primary bg-grey">
+                        <h3 >Premium website design</h3>
+                        <p>Includes:</p>
+                        <ul>
+                            <li>Custom website from scratch</li>
+                            <li>3 custom logos</li>
+                            <li>Discounted web hosting</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-            <h3 >Premium website design</h3>
-            <p>Includes:</p>
-            <ul>
-                <li>Custom website from scratch</li>
-                <li>3 custom logos</li>
-                <li>Discounted web hosting</li>
-            </ul>
+            <div className="row p-5">
+                <div className="col-lg-6">
+                    <div className="p-5 border border-primary bg-grey">
+                        <h3 >Web hosting</h3>
+                        <p>Includes:</p>
+                        <ul>
+                            <li>Free domain</li>
+                            <li>Low monthly rates</li>
+                            <li>Up to 5 different websites</li>
+                        </ul>
+                    </div>
 
-            <h3 >Web hosting</h3>
-            <p>Includes:</p>
-            <ul>
-                <li>Free domain</li>
-                <li>Low monthly rates</li>
-                <li>Up to 5 different websites</li>
-            </ul>
-
-            <h3 >Logo design</h3>
-            <p>Includes:</p>
-            <ul>
-                <li>Custom logo</li>
-                <li>3 different resolutions</li>
-                <li>Up to 3 colours</li>
-            </ul>
+                </div>
+                <div className="col-lg-6">
+                    <div className="p-5 border border-primary bg-grey">
+                        <h3 >Logo design</h3>
+                        <p>Includes:</p>
+                        <ul>
+                            <li>Custom logo</li>
+                            <li>3 different resolutions</li>
+                            <li>Up to 3 colours</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 )
