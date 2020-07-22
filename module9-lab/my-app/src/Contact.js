2476b39a037c312a45b7c8ea404fc993009638db
Alexandre Pineau <alecpineau@gmail.com>
2020-07-21 21:27:46 -0400

    Finished contact page design

--- module9-lab/my-app/src/Contact.js
+++ module9-lab/my-app/src/Contact.js
@@ -3,30 +3,36 @@ import React from 'react';
 export const Contact = () => (
     <div>
         <div className="container">
-            <h2 id="contact">~Contact~</h2>
-            <h3>Street adress</h3>
-            <p>
-                123 MacDonald Road <br />
-                Waterdown, Ontario <br />
-                A0A 0A0 <br />
-            </p>
-
-            <h3>Hours</h3>
-            <ul>
-                <li>Monday: 8-6</li>
-                <li>Tuesday: 8-6</li>
-                <li>Wednesday: 8-6</li>
-                <li>Thursday: 9-5</li>
-                <li>Friday: 9-5</li>
-                <li>Saturday: 9-5</li>
-                <li>Sunday: closed</li>
-            </ul>
-
-            <h3>Email</h3>
-            <p>alphawebdesign@email.com</p>
-
-            <h3>Phone</h3>
-            <p>123-456-7890</p>
+            <div class="row">
+                <div class="col-lg-3">
+                    <h3>Street adress</h3>
+                    <p>
+                        123 MacDonald Road <br />
+                        Waterdown, Ontario <br />
+                        A0A 0A0 <br />
+                    </p>
+                </div>
+                <div class="col-lg-3">
+                    <h3>Hours</h3>
+                    <ul>
+                        <li>Monday: 8-6</li>
+                        <li>Tuesday: 8-6</li>
+                        <li>Wednesday: 8-6</li>
+                        <li>Thursday: 9-5</li>
+                        <li>Friday: 9-5</li>
+                        <li>Saturday: 9-5</li>
+                        <li>Sunday: closed</li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <h3>Email</h3>
+                    <p>alphawebdesign@email.com</p>
+                </div>
+                <div class="col-lg-3">
+                    <h3>Phone</h3>
+                    <p>123-456-7890</p>
+                </div>
+            </div>
         </div>
     </div>
 )
